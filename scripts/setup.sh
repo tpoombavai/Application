123a9155020d95222996450be9f3649eb5c0d7ee
Poombavai Thirugnanasambandam <pt15@illinois.edu>
2024-01-28 21:00:44 -0800

    Add a start script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,5 +6,5 @@ cd ~
 git clone https://github.com/tpoombavai/Cloud_Computing_Assignment_2.git
 cd Cloud_Computing_Assignment_2/
 pip install -r requirements.txt
-python3 app.py
+nohup ./start.sh &
 
