aef5cbc175ad61625ccdedd98731fbeb0e2a4c8b
Poombavai Thirugnanasambandam <pt15@illinois.edu>
2024-01-28 18:58:56 -0800

    add the shell script

---
+++ scripts/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo yum install pip -y
+sudo yum install git -y
+cd ~
+git clone https://github.com/tpoombavai/Cloud_Computing_Assignment_2.git
+cd Cloud_Computing_Assignment_2/
+pip install -r requirements.txt
+python3 app.py
+
