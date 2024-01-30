8fdf58bf02063b84429488d1462edfce56df5854
Poombavai Thirugnanasambandam <pt15@illinois.edu>
2024-01-30 00:29:24 -0800

    Check in the contents of AutoScalingSetup.sh

---
+++ scripts/AutoScalingSetup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo yum install pip -y
+sudo yum install git -y
+cd ~
+git clone https://github.com/tpoombavai/Cloud_Computing_Assignment_2.git
+cd Cloud_Computing_Assignment_2/AutoScalingApp/
+pip install -r requirements.txt
+nohup ./start.sh &
